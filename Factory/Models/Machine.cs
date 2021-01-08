06034a6f9ca798ead953da2a8fb608de303845cd
Eric Stratton <strattonericj@gmail.com>
2021-01-08 08:22:30 -0800

    add models including join model

---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public Machine()
+    {
+      this.Engineers = new HashSet<MachineEngineer>();
+    }
+
+    public int MachineId { get; set; }
+    public string Model { get; set; }
+
+    public ICollection<MachineEngineer> Engineers { get; set; }
+  }
+}
