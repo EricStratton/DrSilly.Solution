a3ee7c00d59a762c3c53df00b78eab6b9ccf8b40
Eric Stratton <strattonericj@gmail.com>
2021-01-10 11:46:18 -0800

    begin rebuilding Locations controller

--- Factory/Controllers/LocationsController.cs
+++ Factory/Controllers/LocationsController.cs
@@ -16,37 +16,22 @@ namespace Factory.Controllers
       _db = db;
     }
 
-    // public ActionResult Index()
-    // {
-    //   List<SelectListLocation> locations = new List<SelectListLocation>();
-    //   locations.Add(new Location
-    //   {
-    //     Facility = "NE",
-    //     LocationId = 1
-    //   });
-    //   locations.Add(new Location
-    //   {
-    //     Facility = "SE",
-    //     LocationId = 2
-    //   });
-    //   locations.Add(new Location
-    //   {
-    //     Facility = "SW",
-    //     LocationId = 3
-    //   });
-    //   var model = new Location()
-    //   {
-    //     Location = locations
-
-    //   };
-    //   ViewData["Index"] = location;
-    //   return View();
-    // }
-
     public ActionResult Index()
     {
       List<Location> model = _db.Locations.ToList();
-      return View();
+          // model.Add(new Location { Facility = "NE", LocationId = 1});
+          // model.Add(new Location { Facility = "SE", LocationId = 2});
+          // model.Add(new Location { Facility = "SW", LocationId = 3});
+      return View(model);
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisLocation = _db.Locations
+          .Include(location => location.Engineers)
+          .ThenInclude(join => join.Engineer)
+          .FirstOrDefault(location => location.LocationId == id);
+      return View(thisLocation);
     }
 
     // public ActionResult AddEngineer(int id) 
