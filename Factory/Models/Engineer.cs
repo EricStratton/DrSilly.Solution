fbfd3e6c669ab5d363c551cb4495f513738a19a3
Eric Stratton <strattonericj@gmail.com>
2021-01-10 13:49:31 -0800

    add Location hashset to Engineer model

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -7,11 +7,13 @@ namespace Factory.Models
     public Engineer()
     {
       this.Machines = new HashSet<MachineEngineer>();
+      this.Locations = new HashSet<EngineerLocation>();
     }
 
     public int EngineerId { get; set; }
     public string Name { get; set; }
 
     public virtual ICollection<MachineEngineer> Machines { get; set; }
+    public ICollection<EngineerLocation> Locations { get; set; }
   }
 }
